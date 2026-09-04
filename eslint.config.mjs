ea857abf3d1f3f9da2adc30e966f1b4bfd93f167
Nate Tillman <tillmanlafa26@gmail.com>
2026-09-03 21:31:02 -0400

    initial

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+import { globalIgnores } from 'eslint/config';
 import { FlatCompat } from '@eslint/eslintrc';
 import { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
@@ -6,4 +7,9 @@ const compat = new FlatCompat({
   baseDirectory: dirname(fileURLToPath(import.meta.url)),
 });
 
-export default compat.extends('next/core-web-vitals');
+const eslintConfig = [
+  globalIgnores(['.next/**', 'node_modules/**', 'coverage/**', 'dist/**']),
+  ...compat.extends('next/core-web-vitals'),
+];
+
+export default eslintConfig;
